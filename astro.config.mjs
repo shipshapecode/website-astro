a6886844abe4c501450f33dc17c67de344770edd
Robert Wagner <rwwagner90@gmail.com>
2023-09-13 14:15:28 -0400

    Start on astro 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -32,7 +32,7 @@ export default defineConfig({
     tailwind({
       // Example: Disable injecting a basic `base.css` import on every page.
       // Useful if you need to define and/or import your own custom `base.css`.
-      config: { applyBaseStyles: false }
+      applyBaseStyles: false
     })
   ],
   markdown: {
