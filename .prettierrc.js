639302b1b2a2c1162c1521e2d16350da47d80868
Robert Wagner <rwwagner90@gmail.com>
2023-09-29 22:52:16 -0400

    Lint fix

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,20 @@
 'use strict';
 
+/** @type {import("prettier").Config} */
 module.exports = {
   arrowParens: 'always',
   astroAllowShorthand: false,
   trailingComma: 'none',
   semi: true,
   singleQuote: true,
-  proseWrap: 'always'
+  proseWrap: 'always',
+  plugins: ['prettier-plugin-astro'],
+  overrides: [
+    {
+      files: '*.astro',
+      options: {
+        parser: 'astro'
+      }
+    }
+  ]
 };
